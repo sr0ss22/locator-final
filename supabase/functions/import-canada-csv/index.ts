ba61d9379eaa7ab7874a2e796352879c7d0c710e
Shaun Ross <shaun.ross@hunterdouglas.com>
2025-12-18 08:04:17 -0500

    [dyad] Fixing a Supabase function deployment error - wrote 1 file(s) + extra files edited outside of Dyad

--- supabase/functions/import-canada-csv/index.ts
+++ supabase/functions/import-canada-csv/index.ts
@@ -1,6 +1,6 @@
-import { serve } from 'https://deno.land/std@0.177.0/http/server.ts';
+import { serve } from 'https://deno.land/std@0.190.0/http/server.ts';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-import { parse } from 'https://deno.land/std@0.177.0/csv/mod.ts';
+import { parse } from 'https://deno.land/std@0.190.0/csv/mod.ts';
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
